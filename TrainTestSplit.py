fcab534a071c9e558e7449db3e6e0e49df3db5c6
yangazure <yangazure@github.com>
2020-11-14 09:51:10 -0500

    Modified TrainTestSplit.py to save the train and test tables in the ProgSnap2 Format

--- TrainTestSplit.py
+++ TrainTestSplit.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from os import path
 from ProgSnap2 import ProgSnap2Dataset, PS2
 
 np.random.seed(27601)
+TARGET_TRAIN_PATH = "Train"
+TARGET_TEST_PATH = "Test"
 
-
-data = ProgSnap2Dataset('data/CodeWorkout')
+data = ProgSnap2Dataset('.')
+train_data = ProgSnap2Dataset('TARGET_TRAIN_PATH')
+test_data = ProgSnap2Dataset('TARGET_TEST_PATH')
 
 all_main_df = data.get_main_table()
-
 codestate_df = data.get_code_states_table()
 subject_df = data.load_link_table('Subject')
 
@@ -19,14 +22,14 @@ train_ID, test_ID = train_test_split(data.get_subject_ids(), test_size=0.2)
 print("Training set students:", train_ID.size, ", test set students:", test_ID.size)
 
 # Getting tables
-all_main_df[all_main_df['SubjectID'].isin(train_ID)].to_csv("Splitted_data/Train/TrainMainTable.csv",index=False) 
-all_main_df[all_main_df['SubjectID'].isin(test_ID)].to_csv("Splitted_data/Test/TestMainTable.csv",index=False)
+all_main_df[all_main_df['SubjectID'].isin(train_ID)].to_csv(train_data.path(ProgSnap2Dataset.MAIN_TABLE_FILE),index=False) 
+all_main_df[all_main_df['SubjectID'].isin(test_ID)].to_csv(test_data.path(ProgSnap2Dataset.MAIN_TABLE_FILE),index=False)
 
-codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['CodeStateID'])].to_csv("Splitted_data/Train/TrainCodeStates.csv",index=False)
-codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['CodeStateID'])].to_csv("Splitted_data/Test/TestCodeStates.csv",index=False)
+codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['CodeStateID'])].to_csv(train_data.path(ProgSnap2Dataset.CODE_STATES_TABLE_FILE),index=False)
+codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['CodeStateID'])].to_csv(test_data.path(ProgSnap2Dataset.CODE_STATES_TABLE_FILE),index=False)
 
-subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['SubjectID'])].to_csv("Splitted_data/Train/TrainSubject.csv", index = False)
-subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['SubjectID'])].to_csv("Splitted_data/Test/TestSubject.csv", index = False)
+subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['SubjectID'])].to_csv(train_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'Subject.csv')), index = False)
+subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['SubjectID'])].to_csv(test_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'Subject.csv')), index = False)
 
 # Generating label for Task 1
 # TODO: This is definitely not the correct way to calculate the median number of attempts, since the Attempt column is
