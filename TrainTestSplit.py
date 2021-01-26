f9fd59b92c84499799b06aae18fbf1911c884577
Poorvaja Penmetsa <ppenmet@ncsu.edu>
2021-01-26 09:35:01 -0800

    Fixed label creation

--- TrainTestSplit.py
+++ TrainTestSplit.py
@@ -25,52 +25,53 @@ print("Training set students:", train_ID.size, ", test set students:", test_ID.s
 # Getting tables
 all_main_df[all_main_df['SubjectID'].isin(train_ID)].to_csv(train_data.path(ProgSnap2Dataset.MAIN_TABLE_FILE),index=False) 
 all_main_df[all_main_df['SubjectID'].isin(test_ID)].to_csv(test_data.path(ProgSnap2Dataset.MAIN_TABLE_FILE),index=False)
-
 codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['CodeStateID'])].to_csv(train_data.path(ProgSnap2Dataset.CODE_STATES_TABLE_FILE),index=False)
 codestate_df[codestate_df['CodeStateID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['CodeStateID'])].to_csv(test_data.path(ProgSnap2Dataset.CODE_STATES_TABLE_FILE),index=False)
-
 subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(train_ID)]['SubjectID'])].to_csv(train_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'Subject.csv')), index = False)
 subject_df[subject_df['SubjectID'].isin(all_main_df[all_main_df['SubjectID'].isin(test_ID)]['SubjectID'])].to_csv(test_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'Subject.csv')), index = False)
 
 # Generating label for Task 1
 
-# dataframe of submitted score
-submitted_df = all_main_df[all_main_df['Score'] == 1]
+# dataframe of successful score
+successful_df = all_main_df[all_main_df['Score'] == 1]
 
-# dataframe of unsubmitted score (get all student and problem ids that aren't from submitted)
-sub_student_prob = set([tuple(i) for i in submitted_df[['SubjectID', 'ProblemID']].values])
-unsub_student_prob = []
+# dataframe of unsuccessful score (get all student and problem ids that aren't from successful)
+succ_student_prob = set([tuple(i) for i in successful_df[['SubjectID', 'ProblemID']].values])
+unsucc_student_prob = []
 for i in range(len(all_main_df)):
     x = all_main_df['SubjectID'][i]
     y = all_main_df['ProblemID'][i]
-    if (x,y) not in sub_student_prob:
-        unsub_student_prob.append(all_main_df.iloc[i].values)
-unsubmitted_df = pd.DataFrame(unsub_student_prob,
+    if (x,y) not in succ_student_prob:
+        unsucc_student_prob.append(all_main_df.iloc[i].values)
+unsuccessful_df = pd.DataFrame(unsucc_student_prob,
                              columns = list(all_main_df.columns))
 
-# order unsubmitted rows and get last row
-group_unsub = unsubmitted_df.groupby(['SubjectID', 'ProblemID'])
-group_unsub_df = group_unsub.apply(
+# order unsuccessful rows and get last row
+group_unsucc = unsuccessful_df.groupby(['SubjectID', 'ProblemID'])
+group_unsucc_df = group_unsucc.apply(
     lambda x: x.sort_values(['Order'], ascending=True).tail(1))
 
-# order submitted rows and get first attempt
-group_sub_df = submitted_df.groupby(['SubjectID', 'ProblemID']).apply(
+# get maximum number of attempts
+max_attempt = all_main_df['Attempt'].max()
+
+# total number of attempts for each unsuccessful problem is maximum number of attempts
+group_unsucc_df['Attempt'] = [max_attempt]* len(group_unsucc_df)
+
+# order successful rows and get first attempt
+group_succ_df = successful_df.groupby(['SubjectID', 'ProblemID']).apply(
     lambda x: x.sort_values(['Order'], ascending=True).head(1))
 
-# concat group_sub group_unsub and shuffle
-group_df = pd.concat([group_unsub_df, group_sub_df],ignore_index=True)
+# concat group_succ and group_unsucc
+group_df = pd.concat([group_unsucc_df, group_succ_df],ignore_index=True)
 
 # reset index
 group_df.reset_index(drop=True, inplace = True)
 
-# shuffle dataframe
-group_df = group_df.sample(frac=1)
-
 # TODO: This is definitely not the correct way to calculate the median number of attempts, since the Attempt column is
-# the attempts number, starting at 1 and going up...
+# the attempts number, starting at 1 and going up... (Fixed)
 
 # calculate median attempts from group_df
-median_problemID = all_main_df.groupby(['ProblemID'])[['Attempt']].median() 
+median_problemID = group_df.groupby(['ProblemID'])[['Attempt']].median() 
 median_problemID.rename(columns = {'Attempt': 'Median_Attempt'}, inplace = True)
 
 # get labels based on median 
@@ -80,6 +81,8 @@ subject_problem_df['StudentLabels'] = [0 if all_main_df['Attempt'][i] > subject_
 
 # storing in train and test
 subject_problem_df[subject_problem_df['SubjectID'].isin(train_ID)].to_csv(
-    train_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'SubjectProblem.csv')), index = False) 
+    train_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'SubjectProblem.csv'))
+    , index = False) 
 subject_problem_df[subject_problem_df['SubjectID'].isin(test_ID)].to_csv(
-    test_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'SubjectProblem.csv')), index = False)
+    test_data.path(path.join(ProgSnap2Dataset.LINK_TABLE_DIR, 'SubjectProblem.csv'))
+    , index = False)
