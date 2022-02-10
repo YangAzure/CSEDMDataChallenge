b7b0c050634309a8ef5ad65e50e4c315c0172af0
Thomas Price <thomaswprice1@gmail.com>
2022-02-10 12:05:59 -0500

    Updated model eval script

--- eval_models.bat
+++ eval_models.bat
@@ -1,4 +1,8 @@
-python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\S19\basic_LR_task1\ .\data\Results\S19\basic_LR_task1
-python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\S19\code_RF_task1\ .\data\Results\S19\code_RF_task1
-python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\F19\basic_LR_task1\ .\data\Results\F19\basic_LR_task1
-python .\CodaLabPackages\Track2Package\program\evaluate.py .\data\Prediction\S19\basic_LR_task2\ .\data\Results\S19\basic_LR_task2
+python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\Phase2\basic_LR_task1\ .\data\Results\Phase2\basic_LR_task1
+python .\CodaLabPackages\Track2Package\program\evaluate.py .\data\Prediction\Phase2\basic_LR_task2\ .\data\Results\Phase2\basic_LR_task2
+
+python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\Phase1\basic_LR_task1\ .\data\Results\Phase1\basic_LR_task1
+python .\CodaLabPackages\Track2Package\program\evaluate.py .\data\Prediction\Phase1\basic_LR_task2\ .\data\Results\Phase1\basic_LR_task2
+
+python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\Phase1\code_RF_task1\ .\data\Results\Phase1\code_RF_task1
+python .\CodaLabPackages\Track1Package\program\evaluate.py .\data\Prediction\Phase2\code_RF_task1\ .\data\Results\Phase2\code_RF_task1
